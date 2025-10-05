6b9f3dd4218b7f4d89762c737369a2254463c553
Aqsa Zulfiqar <aqsazulfiqar0206@gmail.com>
2025-10-05 21:06:18 +0500

    Start authentication system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,14 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(username: params[:session][:username].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Logged in successfully"
+            redirect_to root_path
+        else
+            flash.now[:alert] = "There was something wrong with your login details"
+            render 'new'
+        end
     end
 end
